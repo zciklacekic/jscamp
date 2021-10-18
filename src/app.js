96566086e45e60f25721edb215ff261ae2335bcd
zciklacekic <zciklacekic@gmail.com>
2021-10-19 00:02:11 +0300

    18.Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log("Merhaba Zafer")
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+{
+    let dolarDun= 9.10
+}
+
+console.log(dolarDun)
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Ozel Konut Kredisi"]
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+console.log(konutKredileri)
