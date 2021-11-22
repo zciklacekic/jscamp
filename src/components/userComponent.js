68150ffcfda72d0420c4ee1d7bed7ac778baf971
zciklacekic <zciklacekic@gmail.com>
2021-11-22 23:39:58 +0300

    22.11.2021 Gun Sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -10,13 +11,12 @@ let userService = new UserService(logger1)
 let user1 = new User(1,"Zafer","Ciklacekic","Istanbul")
 let user2 = new User(2,"Engin","Demirog","Ankara")
 
-userService.add(user1)
-userService.add(user2)
+//userService.add(user1)
+//userService.add(user2)
 
 
-console.log(userService.list())
-console.log(userService.getById(1))
-
+//console.log(userService.list())
+//console.log(userService.getById(1))
 
 
 let customer = {id:1,firstName:"Zafer"}
@@ -25,3 +25,14 @@ let customer = {id:1,firstName:"Zafer"}
 customer.lastName = "Ciklacekic"
 
 console.log(customer.lastName)
+
+console.log("----------------------")
+userService.load()
+let customerToAdd = new Customer(10,"Seda","Yilmaz","Ankara","h25")
+customerToAdd.type = "customer"
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log("----------------------")
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,74 @@
+export const users = [
+  {
+    id: 1,
+    firstName: "Zafer",
+    lastName: "Ciklacekic",
+    city: "Istanbul",
+    age: 46,
+    creditCardNumber: "654321",
+    type: "customer",
+  },
+  {
+    id: 2,
+    firstName: "Engin",
+    lastName: "Demirog",
+    city: "Ankara",
+    age: 36,
+    creditCardNumber: "123456",
+    type: "customer",
+  },
+  {
+    id: 3,
+    firstName: "Cansu",
+    lastName: "Degirmen",
+    city: "Istanbul",
+    age: 20,
+    salary: 8000,
+    type: "employee",
+  },
+  {
+    id: 4,
+    firstName: "Mucahit",
+    lastName: "Kaya",
+    city: "Bolu",
+    age: 23,
+    salary: 8500,
+    type: "employee",
+  },
+  {
+    id: 5,
+    firstName: "Eren",
+    lastName: "Arı",
+    city: "Diyarbakır",
+    age: 20,
+    salary: 15000,
+    type: "employe",
+  },
+  {
+    id: 6,
+    firstName: "Sehri olmayan isim",
+    lastName: "Arı",
+    city: "Diyarbakır",
+    age: 20,
+    creditCardNumber: "315000",
+    type: "customer"
+  },
+  {
+    id: 7,
+    firstName: "Yaşı olmayan isim",
+    lastName: "Arı",
+    city: "Diyarbakır",
+    age:23,
+    salary: 15000,
+    type: "employee"
+  },
+  {
+    id: 8,
+    firstName: "Maaşı olmayan isim",
+    lastName: "Arı",
+    city: "Diyarbakır",
+    age: 20,
+    salary: 16000,
+    type: "employee"
+  }
+];
